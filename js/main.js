564ce4f303c727e5b8d8f8f9ee8e9176c7c74baf
Bert Pareyn <pareyn.bert@gmail.com>
2012-11-16 12:56:42 +0100

    Add in autoSuggest example

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 (function() {
 
+    var initAutoSuggestExample = function() {
+        var data = {items: [
+            {
+                value: "mjagger23",
+                name: "Mick Jagger",
+                img: 'images/dummy_users/profile-pic-don.png'
+            }
+        ]};
+        $('#autosuggest_example').autoSuggest(data.items, {
+            selectedItemProp: "name",
+            searchObjProps: "name",
+            formatList: function(data, elem){
+                var my_image = data.image;
+                var new_elem = elem.html('<img src="' + data.img + '" class="gt-force-left" /><div class="autosuggest_name">' + data.name + '</div>');
+                return new_elem;
+            }
+        });
+    };
+
+    $(document).ready(initAutoSuggestExample);
 })();
